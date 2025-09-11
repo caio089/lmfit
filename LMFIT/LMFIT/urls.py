f087ec2fb4301286ef27449a58a0cfbb763f0a04
caio campos <ccamposs2007@gmail.com>
2025-09-11 07:29:17 -0300

    CLOUDINARY

--- LMFIT/LMFIT/urls.py
+++ LMFIT/LMFIT/urls.py
@@ -21,9 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('loja.urls')),
-]
-
-# Servir arquivos de mídia durante o desenvolvimento
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',include('loja.urls')),
+    path('',include('catalogo.urls')),
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
